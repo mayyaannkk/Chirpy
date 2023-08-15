00dce54ae2840566947c0dff4bc39b94bbff91ce
mayank <mayankpant.pant120@gmail.com>
2023-08-15 12:24:41 +0530

    file-server

---
+++ file-server/fileserver.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	corsMux := middlewareCors(mux)
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: corsMux,
+	}
+
+	srv.ListenAndServe()
+}
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
